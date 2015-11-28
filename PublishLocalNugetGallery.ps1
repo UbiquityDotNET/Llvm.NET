e30ae82ff59fdd35d9b3e8e34623bcb0b1590a91
Steve Maillet <smaillet-ms@users.noreply.github.com>
2015-11-27 17:42:04 -0800

    - added additional import to publish powershell script in attempt to reolve errors at build...

--- PublishLocalNugetGallery.ps1
+++ PublishLocalNugetGallery.ps1
@@ -1,5 +1,6 @@
 param([string]$BuildOutputRoot)
 
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 
 Find-Files -SearchPattern "BuildOutput\Nuget\**\*.nupkg" | Write-Verbose
