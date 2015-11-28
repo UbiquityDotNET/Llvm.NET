edf2b90dbf8775dd85a4596ffc63ca903210619b
Steve Maillet <smaillet-ms@users.noreply.github.com>
2015-11-27 16:39:42 -0800

    - updated AttributeSet test - re-configured tests to build the test DLL into platform specific build output locations to simplify multi-platform test runs - converted to using runsettings files for configuring the test runs. (one for each platform target to force the proper runtime environment for an AnyCPU unit test that loads a platform specific DLL)

--- src/Llvm.NETTests/LLVM.NETTests/Values/AttributeSetTests.cs
+++ src/Llvm.NETTests/LLVM.NETTests/Values/AttributeSetTests.cs
@@ -31,31 +31,32 @@ namespace Llvm.NET.Values.Tests
             {
                 var structType = ctx.CreateStructType( "testT", false, ctx.Int32Type, ctx.Int8Type.CreateArrayType( 32 ) );
                 var func = module.AddFunction( "test", ctx.GetFunctionType( ctx.VoidType, structType.CreatePointerType() ) );
+                // shouldn't be created with an attribute
+                Assert.IsFalse( func.Attributes.Has( FunctionAttributeIndex.Function, AttributeKind.AlwaysInline ) );
+
                 var newAttribs = func.Attributes.Add( FunctionAttributeIndex.Function, AttributeKind.AlwaysInline );
+                // creating the new attributeset shouldn't modify the original
+                Assert.IsFalse( func.Attributes.Has( FunctionAttributeIndex.Function, AttributeKind.AlwaysInline ) );
+
                 func.Attributes = newAttribs;
+                // replaced attributes, so function should now have the inline attribute
+                Assert.IsTrue( func.Attributes.Has(FunctionAttributeIndex.Function, AttributeKind.AlwaysInline ) );
+
                 var newParamAttribs = func.Attributes.Add( FunctionAttributeIndex.Parameter0, new AttributeValue(AttributeKind.Alignment, 4 ) );
                 func.Attributes = newParamAttribs;
+                Assert.AreEqual( 4ul, func.Attributes.GetAttributeValue( FunctionAttributeIndex.Parameter0, AttributeKind.Alignment ) );
+
+                // function should still have the inline attribute (e.g. adding the param attribute didn't erase the function attribute)
+                Assert.IsTrue( func.Attributes.Has(FunctionAttributeIndex.Function, AttributeKind.AlwaysInline ) );
+
+                // Add another function attribute
+                func.Attributes = func.Attributes.Add( FunctionAttributeIndex.Function, AttributeKind.NoUnwind );
+                Assert.IsTrue( func.Attributes.Has( FunctionAttributeIndex.Function, AttributeKind.NoUnwind ) );
+
+                // function should still have the previous attributes
                 Assert.IsTrue( func.Attributes.Has(FunctionAttributeIndex.Function, AttributeKind.AlwaysInline ) );
                 Assert.AreEqual( 4ul, func.Attributes.GetAttributeValue( FunctionAttributeIndex.Parameter0, AttributeKind.Alignment ) );
             }
-        }
-
-        [TestMethod( )]
-        public void AddTest1( )
-        {
-            Assert.Inconclusive( );
-        }
-
-        [TestMethod( )]
-        public void AddTest2( )
-        {
-            Assert.Inconclusive( );
-        }
-
-        [TestMethod( )]
-        public void AddTest3( )
-        {
-            Assert.Inconclusive( );
         }
 
         [TestMethod( )]
