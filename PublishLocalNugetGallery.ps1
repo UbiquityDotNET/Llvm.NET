d9b892dc61fc5b296acb35bb065fa496ebd744a8
Steve Maillet <smaillet-ms@users.noreply.github.com>
2015-11-27 17:53:43 -0800

    - removed publish to gallery powershell as a failed experiment. VSO couldn't load the assemblies it imported based on the reference vso tasks on GitHub. A serious lack of docs doesn't help...

--- PublishLocalNugetGallery.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$BuildOutputRoot)
-
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
-
-Find-Files -SearchPattern "BuildOutput\Nuget\**\*.nupkg" | Write-Verbose
