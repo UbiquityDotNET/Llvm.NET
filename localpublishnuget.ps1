af58dd0674f45b5b50bc55eaf6fa63084e19910a
Steve Maillet <smaillet-ms@users.noreply.github.com>
2015-11-27 19:43:02 -0800

    - added new powershell script to copy nuget packages to agent's local gallery

---
+++ localpublishnuget.ps1
@@ -0,0 +1,19 @@
+﻿$pkgs = Get-ChildItem BuildOutput\Nuget\**\*.nupkg
+$srcDir = $env:BUILD_SOURCESDIRECTORY
+if( [String]::IsNullOrWhiteSpace( $srcDir ) )
+{
+    $srcDir = (Get-Location).Path
+}
+
+$relatavieRoot = [System.IO.Path]::Combine( $srcDir, "BuildOutput\Nuget")
+"relativeRoot: " + $relatavieRoot
+Foreach( $pkg in $pkgs )
+{ 
+    $targetFolder = $pkg.DirectoryName.Replace( $relatavieRoot,"c:\Nuget")
+    if( ![System.IO.Directory]::Exists( $targetFolder ) )
+    {
+        [System.IO.Directory]::CreateDirectory( $targetFolder )
+    }
+    $targetFile = [System.IO.Path]::Combine($targetFolder, $pkg.Name )
+    $pkg.CopyTo( $targetFile, $true )
+}
