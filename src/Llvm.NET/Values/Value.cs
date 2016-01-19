3cd008a80ae9032637cd8c012d46c424852b1069
Steve Maillet <smaillet-ms@users.noreply.github.com>
2016-01-19 09:22:47 -0800

    Fixed bug in SetDebugLocation scope checks so it uses the InlinedAtScope (Outermost scope of inlining) to verify scope.

--- src/Llvm.NET/Values/Value.cs
+++ src/Llvm.NET/Values/Value.cs
@@ -387,7 +387,7 @@ namespace Llvm.NET.Values
             var instruction = value as Instructions.Instruction;
             if( instruction != null )
             {
-                if( !location.Scope.SubProgram.Describes( instruction.ContainingBlock.ContainingFunction ) )
+                if( !location.InlinedAtScope.SubProgram.Describes( instruction.ContainingBlock.ContainingFunction ) )
                     throw new ArgumentException( "Location does not describe the function containing the provided instruction", nameof( location ) );
 
                 NativeMethods.SetDILocation( value.ValueHandle, location.MetadataHandle );
