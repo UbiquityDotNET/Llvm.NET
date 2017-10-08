cff66c5a39647ef519a893cd4c05c8a99d373a5f
smaillet <smaillet@users.noreply.github.com>
2017-10-08 15:21:23 -0700

    Added Build support for gh-pages

--- Push-Docs.ps1
+++ Push-Docs.ps1
@@ -3,6 +3,15 @@ if($env:APPVEYOR_PULL_REQUEST_NUMBER)
     return;
 }
 
+# Docs must only be updated from a build with the official repo as the default remote.
+# This ensures that the links to source in the generated docs will have the correct URLs
+# (e.g. docs pushed to the official repo MUST not have links to source in some private fork)
+$remoteUrl = git ls-remote --get-url
+if($remoteUrl -ne "https://github.com/UbiquityDotNET/Llvm.NET.git")
+{
+    throw "Pushing docs is only allowed when the origin remote is the official source release"
+}
+
 if($env:APPVEYOR)
 {
     git config --global credential.helper store
@@ -11,6 +20,23 @@ if($env:APPVEYOR)
     git config --global user.name "$env:docspush_username"
 }
 
-git add docs/**
-git commit -m "[skip ci] CI Docs Update"
-git push origin HEAD:master -q
+pushd .\BuildOutput\docs
+try
+{
+    # prevent line ending conversions
+    git config core.safecrlf false
+    git config core.autocrlf input
+
+    Write-Information "Adding files to git"
+    git add *
+
+    Write-Information "Commiting changes to git"
+    git commit -m "CI Docs Update ($env:APPVEYOR_BUILD_VERSION)"
+
+    Write-Information "pushing changes to git"
+    git push -q
+}
+finally
+{
+    popd
+}
