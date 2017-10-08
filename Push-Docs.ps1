12befa388f8ec4c11ab24865f4a810666fa0cf26
smaillet <smaillet@users.noreply.github.com>
2017-10-07 20:38:02 -0700

    Adding support for building and publidhing the docs on CI builds

---
+++ Push-Docs.ps1
@@ -0,0 +1,16 @@
+if($env:APPVEYOR_PULL_REQUEST_NUMBER)
+{
+    return;
+}
+
+if($env:APPVEYOR)
+{
+    git config --global credential.helper store
+    Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
+    git config --global user.email "Your email"
+    git config --global user.name "Your Name"
+}
+
+git add docs/**
+git commit -m "[skip ci] CI Docs Update"
+git push
