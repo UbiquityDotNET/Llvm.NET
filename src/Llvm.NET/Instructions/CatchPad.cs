81b242a0fba29acaa6e6dc7d3968e277661ce9c8
smaillet <smaillet@users.noreply.github.com>
2017-11-26 16:23:18 -0800

    Fixed doc xref links - docfx doesn't handle xref links with only a directory as root. All links must be to a file or a relative region within the file

--- src/Llvm.NET/Instructions/CatchPad.cs
+++ src/Llvm.NET/Instructions/CatchPad.cs
@@ -13,8 +13,8 @@ namespace Llvm.NET.Instructions
     /// in the block.
     /// </remarks>
     /// <seealso href="xref:llvm_langref#catchpad-instruction">LLVM 'catchpad' Instruction</seealso>
-    /// <seealso href="xref:llvm_releases_docs/ExceptionHandling.html">Exception Handling in LLVM</seealso>
-    /// <seealso href="xref:llvm_releases_docs/ExceptionHandle.html#wineh">Exception Handling using the Windows Runtime</seealso>
+    /// <seealso href="xref:llvm_exception_handling#exception-handling-in-llvm">Exception Handling in LLVM</seealso>
+    /// <seealso href="xref:llvm_exception_handling#wineh">Exception Handling using the Windows Runtime</seealso>
     public class CatchPad
         : FuncletPad
     {
