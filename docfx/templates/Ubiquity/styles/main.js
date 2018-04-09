89fa0d209c34bb7a986acf672a925cb4f58bb691
Steven Maillet <smaillet@users.noreply.github.com>
2018-04-08 18:11:28 -0700

    * Fixed native code nuget pack dependency support by creating a new intermediate SDK project that only builds the package. (#64)
    This allows the other managed projects to use a project reference to get build dependencies correct AND get the correct NuGet dependencies as well.
    * Disabled parallel build in the automated build scripts. Support for native and managed "SDK" projects is tenuous at best and parallel builds result
    in the trying to build the native code from multiple threads/dependency graphs at once. The projects are mostly serially dependent anyway.
    * Added simple highlightJS language support for the Kaleidoscope language

--- docfx/templates/Ubiquity/styles/main.js
+++ docfx/templates/Ubiquity/styles/main.js
@@ -128,44 +128,65 @@ hljs.registerLanguage("llvm", function (e) {
 hljs.registerLanguage("antlr", function (e) {
     return {
         k:
-          'grammar fragment',
-        c: [
-          e.C(
+            'grammar fragment',
+        c: [{
+            cN: "keyword",
+            b: "i\\d+"
+        },
+        e.C(
             '//', '\\n', { r: 0 }
-          ),
-          e.QSM,
-          e.ASM,
-          {
-              cN: 'meta',
-              v: [
+        ),
+        e.QSM,
+        e.ASM,
+        {
+            cN: 'meta',
+            v: [
                 { b: '{', e: '}' },
                 { b: '{', e: '}\?' },
-              ],
-              r: 0
-          },
-          {
-              cN: 'symbol',
-              v: [
+            ],
+            r: 0
+        },
+        {
+            cN: 'symbol',
+            v: [
                 { b: '([A-Z][\\w\\-$.]*)' },
-              ]
-          },
-          {
-              cN: 'number',
-              v: [
-                  { b: '0[xX][a-fA-F0-9]+' },
-                  { b: '-?\\d+(?:[.]\\d+)?(?:[eE][-+]?\\d+(?:[.]\\d+)?)?' }
-              ],
-              r: 0
-          },
-          {
-              cN: 'title',
-              v: [
+            ]
+        },
+        {
+            cN: 'number',
+            v: [
+                { b: '0[xX][a-fA-F0-9]+' },
+                { b: '-?\\d+(?:[.]\\d+)?(?:[eE][-+]?\\d+(?:[.]\\d+)?)?' }
+            ],
+            r: 0
+        },
+        {
+            cN: 'title',
+            v: [
                 { b: '([-a-zA-Z$._][\\w\\-$.]*)' },
-              ]
-          },
+            ]
+        },
 
         ]
     };
-}
-);
+});
+
+// Add Kaleidoscope language support from highlightjs.org
+hljs.registerLanguage("kaleidoscope", function (e) {
+    return {
+        k: 'def extern if then else for in var unary binary',
+        c: [
+            e.C(
+                '#', '\\n', { r: 0 }
+            ),
+            {
+                cN: 'number',
+                v: [
+                    { b: '-?\\d+(?:[.]\\d+)?(?:[eE][-+]?\\d+(?:[.]\\d+)?)?' }
+                ],
+                r: 0
+            }
+        ]
+    };
+});
 
