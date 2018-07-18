22612162093ee5f0e36bf35b04b0a9a7c7a8b577
smaillet <smaillet@users.noreply.github.com>
2018-07-18 00:02:48 -0700

    Updated to build docs after other projects and cache the LLvm LIBs zip file

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -122,6 +122,26 @@ try
             Push-AppveyorArtifact $item.FullName
         }
     }
+
+    if( !(Test-Path (Join-Path $buildPaths.DocsOutput '.git') -PathType Container))
+    {
+        Write-Information "Cloning Docs repository"
+        pushd BuildOutput -ErrorAction Stop
+        try
+        {
+            git clone https://github.com/UbiquityDotNET/Llvm.NET.git -b gh-pages docs -q
+        }
+        finally
+        {
+            popd
+        }
+    }
+
+    Write-Information "Restoring Docs Project"
+    Invoke-MSBuild -Targets Restore -Project docfx\Llvm.NET.DocFX.csproj -Properties $msBuildProperties -LoggerArgs $msbuildLoggerArgs ($msbuildLoggerArgs + @("/bl:Llvm.NET-docfx-restore.binlog") )
+
+    Write-Information "Building Docs"
+    Invoke-MSBuild -Targets Build -Project docfx\Llvm.NET.DocFX.csproj -Properties $msBuildProperties -LoggerArgs $msbuildLoggerArgs ($msbuildLoggerArgs + @("/bl:Llvm.NET-docfx-build.binlog") )
 }
 finally
 {
