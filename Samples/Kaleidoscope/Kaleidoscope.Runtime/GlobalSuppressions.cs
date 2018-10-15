6321e26682e5b1e38a632c108b4ea8b80ded06c5
Steven Maillet <smaillet@users.noreply.github.com>
2018-10-14 20:30:35 -0700

    Added GlobalSuppressions to cleanup the build output for the samples and tests (#79)

--- Samples/Kaleidoscope/Kaleidoscope.Runtime/GlobalSuppressions.cs
+++ Samples/Kaleidoscope/Kaleidoscope.Runtime/GlobalSuppressions.cs
@@ -11,11 +11,7 @@
 using System.Diagnostics.CodeAnalysis;
 
 // until full docs generation for the sample support libraries is enabled, these are just annoying noise
+[assembly: SuppressMessage( "StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Sample Application" )]
 [assembly: SuppressMessage( "StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Sample Application" )]
 [assembly: SuppressMessage( "StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "Sample Application" )]
 [assembly: SuppressMessage( "StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "Sample Application" )]
-[assembly: SuppressMessage( "StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Sample Application" )]
-
-[assembly: SuppressMessage( "Microsoft.Naming", "CA1715", Justification = "Generated code", MessageId = "Result", Scope = "type", Target = "Kaleidoscope.Grammar.KaleidoscopeBaseVisitor`1" )]
-[assembly: SuppressMessage( "Microsoft.Naming", "CA1715", Justification = "Generated code", MessageId = "Result", Scope = "type", Target = "Kaleidoscope.Grammar.IKaleidoscopeVisitor`1" )]
-[assembly: SuppressMessage( "Microsoft.Naming", "CA1708", Justification = "Generated code" )]
