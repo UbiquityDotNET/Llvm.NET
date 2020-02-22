22c30dabb500deebf0c0b8a6d33740e54f35ee1c
smaillet <smaillet@users.noreply.github.com>
2020-02-22 09:41:24 -0800

    Added --allow-empty to commit of docs to avoid non-zero return from empty commit as failure

--- Build-Docs.ps1
+++ Build-Docs.ps1
@@ -70,16 +70,16 @@ try
             Write-Information "Adding files to git"
             git add -A
             git ls-files -o --exclude-standard | %{ git add $_}
-            if(!$?)
+            if($LASTEXITCODE -ne 0)
             {
                 throw "git add failed"
             }
 
             Write-Information "Committing changes to git"
-            git commit -m "CI Docs Update"
-            if(!$?)
+            git commit --allow-empty -m "CI Docs Update"
+            if($LASTEXITCODE -ne 0)
             {
-                throw "git commit failed"
+                throw "git commit failed: Exit code $LASTEXITCODE"
             }
         }
         finally
