f82cf596012a6ac7d8a71b5e2a49c2322d75849d
Steven Maillet <smaillet@users.noreply.github.com>
2020-02-22 15:26:36 -0800

    Added logging for setting credentials (#137)

--- Set-PushCredentials.ps1
+++ Set-PushCredentials.ps1
@@ -4,8 +4,23 @@ Initialize-BuildEnvironment
 
 if( $IsAutomatedBuild -and !$IsPullRequestBuild )
 {
+    if(!$env:docspush_access_token -or !$env:docspush_email -or !$env:docspush_username)
+    {
+        throw "Env vars missing!"
+    }
+
+    # Best effort, on git commands as they can return non-zero even if nothing is wrong.
+    $ErrorActionPreference = Continue
+
+    Write-Information "Setting credential.helper"
     git config --global credential.helper store
+
+    Write-Information "Adding credentials to .git-credentials"
     Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
+
+    Write-Information "Adding user.email configuration"
     git config --global user.email "$env:docspush_email"
+
+    Write-Information "Adding user.name configuration"
     git config --global user.name "$env:docspush_username"
 }
