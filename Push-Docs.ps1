69129b1715d05a34156d12a213e714e97e4af92c
Steven Maillet <smaillet@users.noreply.github.com>
2020-02-23 18:38:40 -0800

    Adding functional support for GH Actions and disabling AppVeyor (#143)
    * Added user and email info to the docs push for GHActions
    * Use cmd to get logging from docs push on GH
    * Added rethrow of exception logged in docs push
    * Use pre-baked push action for docs push
    * Disabled AppVeyor build by renaming the YAML file.
    * Changed GH Action build to operate on master branch

--- Push-Docs.ps1
+++ Push-Docs.ps1
@@ -1,3 +1,7 @@
+Param(
+    [switch]$SkipPush
+)
+
 . .\buildutils.ps1
 
 Initialize-BuildEnvironment
@@ -23,15 +27,35 @@ if(!($remoteUrl -like "https://github.com/UbiquityDotNET/Llvm.NET*"))
     throw "Pushing docs is only allowed when the origin remote is the official source release current remote is '$remoteUrl'"
 }
 
+if(!$env:docspush_access_token -and !$SkipPush)
+{
+    Write-Error "Missing docspush_access_token"
+}
+
+if(!$env:docspush_email)
+{
+    Write-Error "Missing docspush_email"
+}
+
+if(!$env:docspush_username)
+{
+    Write-Error "Missing docspush_username"
+}
+
 pushd .\BuildOutput\docs -ErrorAction Stop
 try
 {
-    git config --global credential.helper store
-    Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
-    git config --global user.email "$env:docspush_email"
-    git config --global user.name "$env:docspush_username"
+    if($env:docspush_access_token)
+    {
+        git config --local credential.helper store
+        Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:docspush_access_token):x-oauth-basic@github.com`n"
+    }
 
-    Write-Information "Adding files to git"
+    git config --local user.email "$env:docspush_email"
+    git config --local user.name "$env:docspush_username"
+
+    # use cmd /c to get message output from GH Actions that are suppressed when a secret is used.
+    cmd /c echo Adding files to git
     git add -A
     git ls-files -o --exclude-standard | %{ git add $_}
     if(!$?)
@@ -39,15 +63,24 @@ try
         throw "git add failed"
     }
 
-    Write-Information "Committing changes to git"
+    cmd /c echo Committing changes to git
     git commit -m "CI Docs Update"
     if(!$?)
     {
         throw "git commit failed"
     }
 
-    Write-Information "pushing changes to git"
-    git push -q
+    if(!$SkipPush)
+    {
+        cmd /c echo Pushing changes to git
+        git push
+    }
+}
+catch
+{
+    cmd /c echo Error pushing docs:
+    cmd /c echo ($_.Exception | out-string)
+    throw
 }
 finally
 {
