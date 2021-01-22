427000256aecc6db9c44c2b78160e65c0798d00c
Stefan J. Wernli <swernli@microsoft.com>
2021-01-21 17:05:03 -0800

    Set tests to do full build and restore

--- PsModules/CommonBuild/CommonBuild.psm1
+++ PsModules/CommonBuild/CommonBuild.psm1
@@ -343,7 +343,7 @@ function Invoke-DotNetTest($buildInfo, $projectRelativePath)
 {
     $testProj = Join-Path $buildInfo['RepoRootPath'] $projectRelativePath
     $runSettings = Join-Path $buildInfo['SrcRootPath'] 'x64.runsettings'
-    $result = dotnet test $testProj -s $runSettings --no-build --no-restore --logger "trx" -r $buildInfo['TestResultsPath'] `
+    $result = dotnet test $testProj -s $runSettings --logger "trx" -r $buildInfo['TestResultsPath'] `
             | Out-String
     Write-Information $result
     return $LASTEXITCODE -ne 0
