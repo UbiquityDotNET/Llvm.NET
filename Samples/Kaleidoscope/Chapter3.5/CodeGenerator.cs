3ac8eb9d9419a2b97df1ec30dbce7e1dd0046af4
smaillet <25911635+smaillet@users.noreply.github.com>
2025-03-13 16:31:37 -0700

    Missed code cleanup (Formating)

--- Samples/Kaleidoscope/Chapter3.5/CodeGenerator.cs
+++ Samples/Kaleidoscope/Chapter3.5/CodeGenerator.cs
@@ -26,37 +26,37 @@ namespace Kaleidoscope.Chapter3_5
         , IDisposable
         , IKaleidoscopeCodeGenerator<Value>
     {
-        public CodeGenerator( DynamicRuntimeState globalState )
+        public CodeGenerator(DynamicRuntimeState globalState)
             : base( null )
         {
             ArgumentNullException.ThrowIfNull( globalState );
 
-            if( globalState.LanguageLevel > LanguageLevel.SimpleExpressions )
+            if(globalState.LanguageLevel > LanguageLevel.SimpleExpressions)
             {
                 throw new ArgumentException( "Language features not supported by this generator", nameof( globalState ) );
             }
 
             RuntimeState = globalState;
-            Context = new Context( );
+            Context = new Context();
             Module = Context.CreateBitcodeModule( "Kaleidoscope" );
             InstructionBuilder = new InstructionBuilder( Context );
         }
 
-        public void Dispose( )
+        public void Dispose()
         {
-            Module.Dispose( );
-            Context.Dispose( );
+            Module.Dispose();
+            Context.Dispose();
         }
 
-        public OptionalValue<Value> Generate( IAstNode ast )
+        public OptionalValue<Value> Generate(IAstNode ast)
         {
-            ArgumentNullException.ThrowIfNull(ast);
+            ArgumentNullException.ThrowIfNull( ast );
 
             // Prototypes, including extern are ignored as AST generation
             // adds them to the RuntimeState so that already has the declarations
             // They are looked up and added to the module as extern if not already
             // present if they are called.
-            if( ast is not FunctionDefinition definition )
+            if(ast is not FunctionDefinition definition)
             {
                 return default;
             }
@@ -65,37 +65,37 @@ namespace Kaleidoscope.Chapter3_5
             return OptionalValue.Create<Value>( function );
         }
 
-        public override Value? Visit( ConstantExpression constant )
+        public override Value? Visit(ConstantExpression constant)
         {
             ArgumentNullException.ThrowIfNull( constant );
 
             return Context.CreateConstant( constant.Value );
         }
 
-        public override Value? Visit( BinaryOperatorExpression binaryOperator )
+        public override Value? Visit(BinaryOperatorExpression binaryOperator)
         {
             ArgumentNullException.ThrowIfNull( binaryOperator );
 
-            switch( binaryOperator.Op )
+            switch(binaryOperator.Op)
             {
             case BuiltInOperatorKind.Less:
-                {
-                    var tmp = InstructionBuilder.Compare( RealPredicate.UnorderedOrLessThan
+            {
+                var tmp = InstructionBuilder.Compare( RealPredicate.UnorderedOrLessThan
                                                         , binaryOperator.Left.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
                                                         , binaryOperator.Right.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
                                                         ).RegisterName( "cmptmp" );
-                    return InstructionBuilder.UIToFPCast( tmp, InstructionBuilder.Context.DoubleType )
-                                             .RegisterName( "booltmp" );
-                }
+                return InstructionBuilder.UIToFPCast( tmp, InstructionBuilder.Context.DoubleType )
+                                         .RegisterName( "booltmp" );
+            }
 
             case BuiltInOperatorKind.Pow:
-                {
-                    var pow = GetOrDeclareFunction( new Prototype( "llvm.pow.f64", "value", "power" ) );
-                    return InstructionBuilder.Call( pow
-                                                  , binaryOperator.Left.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
-                                                  , binaryOperator.Right.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
-                                                  ).RegisterName( "powtmp" );
-                }
+            {
+                var pow = GetOrDeclareFunction( new Prototype( "llvm.pow.f64", "value", "power" ) );
+                return InstructionBuilder.Call( pow
+                                              , binaryOperator.Left.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
+                                              , binaryOperator.Right.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
+                                              ).RegisterName( "powtmp" );
+            }
 
             case BuiltInOperatorKind.Add:
                 return InstructionBuilder.FAdd( binaryOperator.Left.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidExpr )
@@ -122,18 +122,18 @@ namespace Kaleidoscope.Chapter3_5
             }
         }
 
-        public override Value? Visit( FunctionCallExpression functionCall )
+        public override Value? Visit(FunctionCallExpression functionCall)
         {
             ArgumentNullException.ThrowIfNull( functionCall );
 
             string targetName = functionCall.FunctionPrototype.Name;
 
             Function? function;
-            if( RuntimeState.FunctionDeclarations.TryGetValue( targetName, out Prototype? target ) )
+            if(RuntimeState.FunctionDeclarations.TryGetValue( targetName, out Prototype? target ))
             {
                 function = GetOrDeclareFunction( target );
             }
-            else if( !Module.TryGetFunction( targetName, out function ) )
+            else if(!Module.TryGetFunction( targetName, out function ))
             {
                 throw new CodeGeneratorException( $"Definition for function {targetName} not found" );
             }
@@ -146,12 +146,12 @@ namespace Kaleidoscope.Chapter3_5
         }
 
         #region FunctionDefinition
-        public override Value? Visit( FunctionDefinition definition )
+        public override Value? Visit(FunctionDefinition definition)
         {
             ArgumentNullException.ThrowIfNull( definition );
 
             var function = GetOrDeclareFunction( definition.Signature );
-            if( !function.IsDeclaration )
+            if(!function.IsDeclaration)
             {
                 throw new CodeGeneratorException( $"Function {function.Name} cannot be redefined in the same module" );
             }
@@ -160,33 +160,33 @@ namespace Kaleidoscope.Chapter3_5
             {
                 var entryBlock = function.AppendBasicBlock( "entry" );
                 InstructionBuilder.PositionAtEnd( entryBlock );
-                NamedValues.Clear( );
-                foreach( var param in definition.Signature.Parameters )
+                NamedValues.Clear();
+                foreach(var param in definition.Signature.Parameters)
                 {
                     NamedValues[ param.Name ] = function.Parameters[ param.Index ];
                 }
 
                 var funcReturn = definition.Body.Accept( this ) ?? throw new CodeGeneratorException( ExpectValidFunc );
                 InstructionBuilder.Return( funcReturn );
-                function.Verify( );
+                function.Verify();
 
                 // pass pipeline is run against the module
                 var errInfo = function.ParentModule.TryRunPasses( PassNames );
-                return errInfo.Success ? (Value)function : throw new CodeGeneratorException(errInfo.ToString());
+                return errInfo.Success ? (Value)function : throw new CodeGeneratorException( errInfo.ToString() );
             }
-            catch( CodeGeneratorException )
+            catch(CodeGeneratorException)
             {
-                function.EraseFromParent( );
+                function.EraseFromParent();
                 throw;
             }
         }
         #endregion
 
-        public override Value? Visit( VariableReferenceExpression reference )
+        public override Value? Visit(VariableReferenceExpression reference)
         {
             ArgumentNullException.ThrowIfNull( reference );
 
-            if( !NamedValues.TryGetValue( reference.Name, out Value? value ) )
+            if(!NamedValues.TryGetValue( reference.Name, out Value? value ))
             {
                 // Source input is validated by the parser and AstBuilder, therefore
                 // this is the result of an internal error in the generator rather
@@ -201,19 +201,19 @@ namespace Kaleidoscope.Chapter3_5
 
         // Retrieves a Function for a prototype from the current module if it exists,
         // otherwise declares the function and returns the newly declared function.
-        private Function GetOrDeclareFunction( Prototype prototype )
+        private Function GetOrDeclareFunction(Prototype prototype)
         {
-            if( Module.TryGetFunction( prototype.Name, out Function? function ) )
+            if(Module.TryGetFunction( prototype.Name, out Function? function ))
             {
                 return function;
             }
 
             var llvmSignature = Context.GetFunctionType( Context.DoubleType, prototype.Parameters.Select( _ => Context.DoubleType ) );
             var retVal = Module.CreateFunction( prototype.Name, llvmSignature );
-            retVal.AddAttribute(FunctionAttributeIndex.Function, prototype.IsExtern ? AttributeKind.BuiltIn : AttributeKind.NoBuiltIn);
+            retVal.AddAttribute( FunctionAttributeIndex.Function, prototype.IsExtern ? AttributeKind.BuiltIn : AttributeKind.NoBuiltIn );
 
             int index = 0;
-            foreach( var argId in prototype.Parameters )
+            foreach(var argId in prototype.Parameters)
             {
                 retVal.Parameters[ index ].Name = argId.Name;
                 ++index;
