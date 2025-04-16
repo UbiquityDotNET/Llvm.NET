05cd864cac5e0271d79a7fbf8342bac435005f09
smaillet <25911635+smaillet@users.noreply.github.com>
2025-04-16 12:26:04 -0700

    Added runtime message formatting

---
+++ src/Ubiquity.NET.Runtime.Utils/MsBuildMessageInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Ubiquity.NET.Runtime.Utils
+{
+    /// <summary>MSBUILD/VS message information holder</summary>
+    /// <seealso href="https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-diagnostic-format-for-tasks?view=vs-2022"/>
+    public readonly record struct MsBuildMessageInfo
+    {
+        /// <summary>Initializes a new instance of the <see cref="MsBuildMessageInfo"/> struct</summary>
+        /// <param name="origin">Origin of the message</param>
+        /// <param name="location">Location in source for the origin of this message</param>
+        /// <param name="subcategory">Subcategory of the message</param>
+        /// <param name="category">Category of the message</param>
+        /// <param name="code">Code for the message (No spaces)</param>
+        /// <param name="msgText">Text of the message</param>
+        /// <remarks>
+        /// <paramref name="code"/> must NOT be localized. It is required to universally identify a particular message.
+        /// <paramref name="msgText"/>should be localized if the source tool supports localization.
+        /// </remarks>
+        /// <exception cref="ArgumentException"></exception>
+        public MsBuildMessageInfo(
+            string origin,
+            SourceLocation? location,
+            string? subcategory,
+            MsgCategory category,
+            string? code,
+            string msgText)
+        {
+            ArgumentNullException.ThrowIfNull(origin);
+            ArgumentException.ThrowIfNullOrWhiteSpace(msgText);
+            if( code is not null && code.Any((c)=>char.IsWhiteSpace(c)))
+            {
+                throw new ArgumentException("code must not contain whitespace", nameof(code));
+            }
+
+            Origin = origin;
+            Subcategory = subcategory;
+            Category = category;
+            Code = code;
+            Text = msgText;
+            Location = location;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString( )
+        {
+            string locString = string.Empty;
+            if(Location is not null)
+            {
+                locString = Location.Value.ToString("B", null);
+            }
+
+            // account for optional values with leading space.
+            string subCat = Subcategory is not null ? $" {Subcategory}" : string.Empty;
+            string code = Code is not null ? $" {Code}" : string.Empty;
+
+            return $"{Origin}{locString} :{subCat} {Category}{code} : {Text}";
+        }
+
+        private readonly string Origin;
+        private readonly string? Subcategory;
+        private readonly MsgCategory Category;
+        private readonly string? Code;
+        private readonly string Text;
+        private readonly SourceLocation? Location;
+    }
+}
--- src/Ubiquity.NET.Runtime.Utils/SourceLocation.cs
+++ src/Ubiquity.NET.Runtime.Utils/SourceLocation.cs
@@ -4,6 +4,10 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
 namespace Ubiquity.NET.Runtime.Utils
 {
     /// <summary>Abstraction to hold a source location</summary>
@@ -12,6 +16,7 @@ namespace Ubiquity.NET.Runtime.Utils
     /// <param name="EndLine">Ending line number of the location [1..n]</param>
     /// <param name="EndColumn">Ending column position of the location [0..n-1]</param>
     public readonly record struct SourceLocation( int StartLine, int StartColumn, int EndLine, int EndColumn )
+        : IFormattable
     {
         /// <inheritdoc/>
         public override string ToString( )
@@ -19,6 +24,51 @@ namespace Ubiquity.NET.Runtime.Utils
             return StartLine == EndLine && StartColumn == EndColumn
                 ? $"({StartLine},{StartColumn})"
                 : $"({StartLine},{StartColumn},{EndLine},{EndColumn})";
+        }
+
+        /// <inheritdoc/>
+        /// <remarks>
+        /// Accepted format strings are:
+        /// "B" for MSBuild format used for Windows build tools.
+        /// "R" for runtime specific default ("B" for Windows)
+        /// [Format strings for other runtimes TBD]
+        /// </remarks>
+        public string ToString( string? format, IFormatProvider? formatProvider )
+        {
+            formatProvider ??= CultureInfo.CurrentCulture;
+            return format switch
+            {
+                "B" => FormatMsBuild(formatProvider),
+                "R" => FormatMsBuild(formatProvider), // TODO: Adjust this to select format based on current runtime
+                _ => ToString()
+            };
+        }
+
+        // SEE: https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-diagnostic-format-for-tasks?view=vs-2022
+        [SuppressMessage( "Style", "IDE0046:Convert to conditional expression", Justification = "Result is NOT simpler" )]
+        private string FormatMsBuild( IFormatProvider formatProvider )
+        {
+            if(StartLine == 0)
+            {
+                return string.Empty;
+            }
+
+            if(EndLine == 0)
+            {
+                return StartColumn == 0
+                     ? $"({StartLine}"
+                     : $"({StartLine}, {StartColumn})";
+            }
+            else if( EndLine == StartLine)
+            {
+                return $"({StartLine}, {StartColumn}-{EndColumn})";
+            }
+            else
+            {
+                return StartColumn == 0 && EndColumn == 0
+                     ? $"({StartLine}-{EndLine})"
+                     : $"({StartLine}, {StartColumn}, {EndLine}, {EndColumn})";
+            }
         }
     }
 }
