511f775021070005599ff70df90e8203a6fdeabf
smaillet <25911635+smaillet@users.noreply.github.com>
2025-05-06 10:08:59 -0700

    Updated antlr highlighter to better reflect language

--- docfx/templates/Ubiquity/public/antlr.js
+++ docfx/templates/Ubiquity/public/antlr.js
@@ -4,58 +4,108 @@ Simple antlr syntax highlighting for Highlight.JS
 
 export function antlr(hljs)
 {
-    return {
-        aliases: ['antlr'],
+    // lexer rules always begin with uppercase character
+    const lexerIdent_RE = '\\b[A-Z]' + hljs.UNDERSCORE_IDENT_RE + '\\b';
+    const lexerIdent = {
+        scope: 'LexerRuleName',
+        begin: lexerIdent_RE,
+        relevance: 0
+    };
+
+    // grammar rules always begin with a lowercase character
+    const grammarIdent_RE = '\\b[a-z]' + hljs.UNDERSCORE_IDENT_RE + '\\b';
+    const grammarIdent = {
+        scope: 'GrammarRuleName',
+        begin: grammarIdent_RE,
+        relevance: 0
+    };
+
+    const semanticPredicate = {
+        scope: 'SemanticPredicate',
+        begin: '{',
+        end: /}\?/,
+    };
+
+    const lexerCommand = {
+        scope: 'LexerCommand',
+        begin: '->',
+        beginScope: 'punctuation',
+        end: ';',
+        endScope: 'punctuation',
+        endsParent: true,
         keywords: {
-            keyword: 'grammar fragment'
+            built_in: 'skip more popMode mode pushMode type channel'
+        }
+    };
+
+    const lexerRule = {
+        scope: 'LexerRule',
+        begin: [lexerIdent_RE, '\\s*', ':', '\\s*'],
+        beginScope: {
+            1: 'LexerRuleName',
+            3: 'punctuation',
+        },
+        end: ';',
+        endScope: 'punctuation',
+        relevance: 10,
+        contains: [
+            semanticPredicate,
+            lexerCommand,
+            lexerIdent,
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.APOS_STRING_MODE,
+            hljs.QUOTE_STRING_MODE,
+            hljs.BACKSLASH_ESCAPE,
+        ]
+    };
+
+    const grammarAltName = {
+        begin: "#",
+        end: /\s*$/,
+        contains: [
+            {
+                scope: 'GrammarAltName',
+                begin: hljs.UNDERSCORE_IDENT_RE,
+            }
+        ]
+    };
+
+    const grammarRule = {
+        scope: 'GrammarRule',
+        begin: [grammarIdent_RE, '\\s*', ':', '\\s*'],
+        beginScope: {
+            1: "GrammarRuleName",
+            3: "punctuation",
+        },
+        end: ';',
+        endScope: 'punctuation',
+        relevance: 10,
+        contains: [
+            grammarAltName,
+            semanticPredicate,
+            lexerIdent,
+            grammarIdent,
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.APOS_STRING_MODE,
+            hljs.QUOTE_STRING_MODE,
+            hljs.C_NUMBER_MODE,
+        ]
+    };
+
+    return {
+        name: "ANTLR V4 (.g4)",
+        aliases: ['antlr'],
+        keywords: ['grammar', 'fragment', 'skip'],
+        classNameAliases: {
+            LexerRuleName: 'type',
+            GrammarRuleName: 'type',
+            GrammarAltName: 'function',
+            // TODO: Fill these in once tagging is complete.
         },
         contains: [
-            hljs.COMMENT('//', '\\n', { relevance: 0 }),
-            {
-                scope: 'regexp',
-                variants: [
-                    { begin: '\\[', end: '\\]' }
-                ],
-                relevance: 0
-            },
-            {
-                scope: 'string',
-                variants: [
-                    { begin: '\'', end: '\'' },
-                    { begin: '"', end: '"' }
-                ],
-                relevance: 0
-            },
-            {
-                scope: 'keyword',
-                variants: [
-                    { begin: 'i\\d+' }
-                ],
-                relevance: 0
-            },
-            {
-                scope: 'meta',
-                variants: [
-                    { begin: '{', end: '}' },
-                    { begin: '{', end: '}\?' }
-                ],
-                relevance: 0
-            },
-            {
-                scope: 'number',
-                variants: [
-                    { begin: '0[xX][a-fA-F0-9]+' },
-                    { begin: '-?\\d+(?:[.]\\d+)?(?:[eE][-+]?\\d+(?:[.]\\d+)?)?' }
-                ],
-                relevance: 0
-            },
-            {
-                scope: 'symbol',
-                variants: [
-                    { begin: '([A-Z][\\w\\-$.]*)' }
-                ],
-                relevance: 0
-            }
+            hljs.C_LINE_COMMENT_MODE,
+            lexerRule,
+            grammarRule,
         ]
     };
 }
