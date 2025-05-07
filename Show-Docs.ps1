aa6d4d58388b225f97b873e0bc5bcb742046f1b4
Steven Maillet <smaillet@users.noreply.github.com>
2025-05-07 09:45:41 -0700

    Docs Script/Tooling update (#245)
    * Added `Show-Docs.ps1` script to serve/host docs from local repo or another path.
        - Another path is useful for serving docs downloaded from GH actions before formal publication to ensure they are building correctly.
    * Added grey background to SVG images for dark theme to provide greater visibility of lines and drop shadows in parse diagrams.

---
+++ Show-Docs.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+    Shows docs using the docfx built-in server
+
+.PARAMETER DocsPathToHost
+    Path of docs to host. Default is the local build but any legit path is accepted.
+    This is useful when downloading docs build artifacts to ensure they are built correctly.
+
+.PARAMETER buildInfo
+   BuildInfo to use for current repo build. The hashtable provided for this must include the
+   `DocsOutputPath` value for the path to use. This is normally set by a call to `Initialize-Environment`
+#>
+Param(
+    [Parameter(Mandatory, ParameterSetName = 'UsePath', Position = 0)]
+    [string]$DocsPathToHost,
+
+    [Parameter(Mandatory, ParameterSetName = 'UseRepo', Position = 0)]
+    [hashtable]$buildInfo
+)
+
+$docFXToolVersion = '2.78.3'
+
+$InformationPreference = 'Continue'
+$ErrorInformationPreference = 'Stop'
+
+Push-Location $PSScriptRoot
+$oldPath = $env:Path
+try
+{
+    . ./repo-buildutils.ps1
+
+    if ($PSCmdlet.ParameterSetName -eq 'UseRepo')
+    {
+        $DocsPathToHost = $buildInfo['DocsOutputPath']
+    }
+
+    if (!(Test-Path $DocsPathToHost -PathType Container))
+    {
+        throw "Path '$DocsPathToHost' does not exist or is not a directory"
+    }
+
+    # make sure the supported tool is installed.
+    Invoke-External dotnet tool install --global docfx --version $docFXToolVersion | Out-Null
+    Invoke-External docfx serve '--open-browser' $DocsPathToHost
+}
+catch
+{
+    # Everything from the official docs to the various articles in the blog-sphere says this isn't needed
+    # and in fact it is redundant - They're all WRONG! By re-throwing the exception the original location
+    # information is retained and the error reported will include the correct source file and line number
+    # data for the error. Without this, only the error message is retained and the location information is
+    # Line 1, Column 1, of the outer most script file, which is, of course, completely useless.
+    throw
+}
+finally
+{
+    Pop-Location
+    $env:Path = $oldPath
+}
